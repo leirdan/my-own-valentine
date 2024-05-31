f1f0c1a9aa24dafc2e0fa590263eb66f9b152591
leirdan <uouleirdan@gmail.com>
2024-05-31 19:42:24 -0300

    :sparkles: feat: Add more interactive texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,62 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { questions, noTexts, yesTexts } from './db.ts';
 
-function App() {
-  const questions: Array<string> = ["Oii, quer sair comigo essa sexta?", "Oxe, pq não?", "Por favooooor :(", "Você é tão chato..", "cuida gatinho deixa de ser fresco rapaz ora ≧◠ᴥ◠≦✊", "pufavoooooo", ":(", "você me obriga a fazer algo...", "Quer sair comigo sexta?"]
-  const noTexts: Array<string> = ["Não", "Porque não.", "Jamais", "Eu sei mas o chato é vc!", "Quero nem ver você pintado de ouro, pilantra", "Não vooooou", "pode chorar", "o quê ?"];
-
+const App = () => {
   const [count, setCount] = useState(0);
   const [yesSize, setYesSize] = useState<number>(14);
-  const [noSize, setNoSize] = useState<number>(14);
+  const [yesBtnVisible, setYesBtnVisible] = useState<boolean>(true);
+  const [noBtnVisibile, setNoBtnVisibile] = useState<boolean>(true);
+  const [yesText, setYesText] = useState<string>(yesTexts[0]);
   const [noText, setNoText] = useState<string>(noTexts[0]);
-  const [questionText, setQuestionText] = useState<string>(questions[0]);
+  const [mainText, setMainText] = useState<string>(questions[0]);
 
   const handleYesClick = () => {
+    setYesBtnVisible(false);
+    setNoBtnVisibile(false);
+    setMainText("obaaa, te amos muitos!!");
   }
 
   const handleNoClick = () => {
     setCount(count + 1);
-    if (count >= noTexts.length - 1) { setCount(0) }
     setYesSize(yesSize + 4)
     setNoText(noTexts[count])
-    setQuestionText(questions[count]);
+    setYesText(yesTexts[count])
+    setMainText(questions[count]);
   }
 
   useEffect(() => {
-    if (questionText === questions[questions.length - 1]) { 
-      // display none on 'no' button
+    if (count == questions.length - 1) {
+      setYesBtnVisible(false);
+      setNoBtnVisibile(true);
     }
-  }, [questionText]);
+    else if (count == questions.length) {
+      setYesBtnVisible(true);
+      setNoBtnVisibile(false);
+    }
+    else if (count > questions.length) { 
+      setYesBtnVisible(false);
+      setNoBtnVisibile(false);
+    }
+  }, [mainText]);
 
   return (
     <>
       <h1>CONVITE</h1>
-      <h3> {questionText} </h3>
+      <h3> {mainText} </h3>
       <div style={{
         padding: '2em',
         display: 'flex',
         justifyContent: 'space-evenly'
       }}>
         <button style={{
+          display: yesBtnVisible || (yesText !== "") ? "block" : "none",
           fontSize: yesSize + 'px'
-        }} onClick={handleYesClick}> Sim </button>
+        }} onClick={handleYesClick}> {yesText} </button>
         <button style={{
-          fontSize: noSize + 'px'
-        }} onClick={handleNoClick}>
+          display: noBtnVisibile ? "block" : "none"
+        }} onClick={handleNoClick}
+        >
           {noText}
         </button>
       </div>
---
+++ src/db.ts
@@ -0,0 +1,5 @@
+export const questions: Array<string> = ["Oii, quer sair comigo essa sexta?", "Oxe, pq não?", "Por favooooor :(", "Você é tão chato..", "cuida gatinho deixa de ser fresco rapaz ora ≧◠ᴥ◠≦✊", "pufavoooooo", ":(", "você me obriga a fazer algo...", "Quer sair comigo sexta? ;)"]
+
+export const noTexts: Array<string> = ["Não", "Porque não.", "Jamais", "Eu sei mas o chato é vc!", "Quero nem ver você pintado de ouro, pilantra", "Não vooooou", "pode chorar", "o quê ?"];
+
+export const yesTexts: Array<string> = ["Sim", "Eu quero", "Siiiim", "Te amo bora sair", "to brincando gatinho, bora", "Vamos!!", "Desculpe, vamos :)", "", "Claro!"];
